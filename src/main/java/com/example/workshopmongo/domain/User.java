baa487261ee69fc2b4ae12bb716e83b64d2cca79
Fabiano Ferro <fabianoferro10Qgmail.com>
2021-09-14 14:37:17 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/example/workshopmongo/domain/User.java
+++ src/main/java/com/example/workshopmongo/domain/User.java
@@ -2,6 +2,9 @@ package com.example.workshopmongo.domain;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,17 +12,19 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode
+@Document
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
-	private String name;
+	@Id
 	private String id;
+	private String name;	
 	private String email;
 	
 	public User() {};	
 	
-	public User(String name, String id, String email) {
+	public User(String id, String name, String email) {
 		super();
 		this.name = name;
 		this.id = id;
